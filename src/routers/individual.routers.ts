9d01115cb932adf7d8d9656382c19eacda430aa9
Eliran <eliran0220@gmail.com>
2022-02-06 16:14:59 +0200

    Reversed individual api

--- src/routers/individual.routers.ts
+++ src/routers/individual.routers.ts
@@ -1,6 +1,6 @@
 import errorWrapper from '../utils/helpers.utils.js';
 import express from 'express';
-//import individualController from '../controllers/individual.controllers.js';
+import individualController from '../controllers/individual.controllers.js';
 
 class IndividualRouter {
   private _router = express.Router();
@@ -10,12 +10,12 @@ class IndividualRouter {
   }
 
   initRouting() {
-    // this._router.post('/', errorWrapper(individualController.createIndividualAcc));
-    // this._router.get('/', errorWrapper(individualController.getAllIndividualsAcc)); 
-    // this._router.get('/:id', errorWrapper(individualController.getIndividualAccById));
-    // this._router.delete('/:id', errorWrapper(individualController.deleteIndividualAccById));
-    // this._router.put('/:id', errorWrapper(individualController.updateIndividualAccById));
-    // this._router.patch('/:id', errorWrapper(individualController.patchIndividualAccById));
+    this._router.post('/', errorWrapper(individualController.createIndividualAcc));
+    this._router.get('/', errorWrapper(individualController.getAllIndividualsAcc)); 
+    this._router.get('/:id', errorWrapper(individualController.getIndividualAccById));
+    this._router.delete('/:id', errorWrapper(individualController.deleteIndividualAccById));
+    this._router.put('/:id', errorWrapper(individualController.updateIndividualAccById));
+    this._router.patch('/:id', errorWrapper(individualController.patchIndividualAccById));
   }
     
   get router(){
