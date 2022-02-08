be7ed2f5f6d1971e4380c8bc41b844419314292f
Eliran <eliran0220@gmail.com>
2022-02-08 16:00:44 +0200

    Commiting current changes before merge with buisness branch

--- src/repositories/SQLRepository/individual.repository.ts
+++ src/repositories/SQLRepository/individual.repository.ts
@@ -1,12 +1,12 @@
-import {ResultSetHeader, RowDataPacket } from 'mysql2';
-import {ServerException } from '../../exceptions/ServerExcpetion.exceptions.js';
-import {IIndividualAccountDTO, IIndividualAccountRecord } from '../../types/dto_models.types.js';
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
+import { ServerException } from '../../exceptions/ServerExcpetion.exceptions.js';
+import { IIndividualAccountDTO, IIndividualAccountRecord } from '../../types/dto_models.types.js';
 import { db } from '../../utils/initializer.utils.js';
-import {RowPacketInfo} from '../../types/builder.types.js';
-import {getIndividualById} from "../../services/individual.services.js";
+import { RowPacketInfo } from '../../types/builder.types.js';
+import { getIndividualById } from '../../services/individual.services.js';
 export async function getIndividualAccountById(id : number) : Promise<RowPacketInfo[]>{
   const [account] = await db.query(
-    'SELECT * FROM individual_account ia JOIN account a ON ia.account_id = a.account_id JOIN address ad ON ia.address_id =ad.address_id WHERE individual_account_id = ?',id
+    'SELECT * FROM individual_account ia JOIN account a ON ia.account_id = a.account_id JOIN address ad ON ia.address_id =ad.address_id WHERE individual_account_id = ?', id,
   ) as RowDataPacket[];
   if (!account) throw new ServerException(`Individual with id ${id} doesn't exists.`);
   return account as RowDataPacket[];
@@ -23,16 +23,16 @@ export async function createIndividualAccount(payload : IIndividualAccountRecord
 
 export async function getAllIndividualsAcc() : Promise<RowPacketInfo[]> {
   const [accounts] = await db.query(
-    'SELECT * FROM individual_account ia JOIN account a ON ia.account_id = a.account_id JOIN address ad ON ia.address_id =ad.address_id'
+    'SELECT * FROM individual_account ia JOIN account a ON ia.account_id = a.account_id JOIN address ad ON ia.address_id =ad.address_id',
   ) as RowDataPacket[];
   return accounts as RowPacketInfo[];
 }
 
 export async function deleteIndividualAccById(id : number) : Promise<RowPacketInfo[]> {
-  const deleted_account = await getIndividualAccountById(id);   
+  const deletedAccount = await getIndividualAccountById(id);   
   const [result] = await db.query(
-    `DELETE FROM individual_account WHERE individual_account_id = ?`
+    'DELETE FROM individual_account WHERE individual_account_id = ?',
   );
-  return deleted_account as RowDataPacket[];
+  return deletedAccount as RowDataPacket[];
 }
 
--- src/types/builder.types.ts
+++ src/types/builder.types.ts
@@ -1,35 +1,35 @@
-import { IIndividualAccountDTO } from "./dto_models.types";
+import { IIndividualAccountDTO } from './dto_models.types';
 
 export interface RowPacketInfo {
-    [key : string] : any
+  [key : string] : any
 }
 
 export function buildIndividualAccountsFromDB(individualAccDto : RowPacketInfo[]) : IIndividualAccountDTO[] {
-    const accounts : IIndividualAccountDTO[] = [];
-    for (const element of individualAccDto) {
-        const account : IIndividualAccountDTO = {
-            individual_account_id : element.individual_account_id,
-            address_id : element.address_id,
-            account_id : element.account_id,
-            individual_id : element.individual_id,
-            first_name : element.first_name,
-            last_name : element.last_name,
-            email : element.email,
-            currency : element.currency,
-            balance : element.balance,
-            status_id : element.status_id,
-            type_name : element.type_name,
-            address : {
-                street_name : element.street_name,
-                street_number : element.street_number,
-                postal_code : element.postal_code,
-                country_code : element.country_code,
-                country_name : element.country_name,
-                city : element.city,
-                region : element.region
-            },
-        }
-        accounts.push(account);
+  const accounts : IIndividualAccountDTO[] = [];
+  for (const element of individualAccDto) {
+    const account : IIndividualAccountDTO = {
+      individual_account_id : element.individual_account_id,
+      address_id : element.address_id,
+      account_id : element.account_id,
+      individual_id : element.individual_id,
+      first_name : element.first_name,
+      last_name : element.last_name,
+      email : element.email,
+      currency : element.currency,
+      balance : element.balance,
+      status_id : element.status_id,
+      type_name : element.type_name,
+      address : {
+        street_name : element.street_name,
+        street_number : element.street_number,
+        postal_code : element.postal_code,
+        country_code : element.country_code,
+        country_name : element.country_name,
+        city : element.city,
+        region : element.region,
+      },
     };
-    return accounts as IIndividualAccountDTO[];
+    accounts.push(account);
+  }
+  return accounts as IIndividualAccountDTO[];
 }
